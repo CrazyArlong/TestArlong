77efa18947cc2daafb751bfc15cc22aa2eef50f5
CrazyArlong <56097769+CrazyArlong@users.noreply.github.com>
2024-12-02 19:17:44 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,3 +4,14 @@ plugins {
     alias(libs.plugins.jetbrains.kotlin.android) apply false
     alias(libs.plugins.android.library) apply false
 }
+
+publishing {
+    publications {
+        create("maven_public", MavenPublication::class) {
+            groupId = "org.gradle.sample"
+            artifactId = "library"
+            version = "1.1"
+            from(components.getByName("java"))
+        }
+    }
+}
