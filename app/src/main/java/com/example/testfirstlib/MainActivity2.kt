aacda5f8fc16ffb005fc48c0e8f19f8b68f3ab75
CrazyArlong <56097769+CrazyArlong@users.noreply.github.com>
2024-12-02 18:23:49 +0800

    111
    222

---
+++ app/src/main/java/com/example/testfirstlib/MainActivity2.kt
@@ -0,0 +1,22 @@
+package com.example.testfirstlib
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.msglib.MsgUtils
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.button)
+
+        button.setOnClickListener{
+            MsgUtils.showShortMsg(this,"Hello Qing!")
+        }
+    }
+}
